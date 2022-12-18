0b84fae3431112e116c561356b00f260bc3f8c5b
ryan <kenidyryan@gmail.com>
2022-12-18 15:07:49 +0700

    user birthdate and place

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload, {
         privateKey: jwtConstants.secret,
-        expiresIn: '600s',
+        // expiresIn: '600s',
       }),
     };
   }
